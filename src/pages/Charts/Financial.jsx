e546b1c5d9cc092c23e4be03b18f217f72563577
blinky1994 <56903269+blinky1994@users.noreply.github.com>
2022-09-01 00:45:47 +0800

    Added more charts

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, 
+  Inject, HiloSeries, Category, Tooltip, Zoom, 
+  Crosshair, Legend } from '@syncfusion/ej2-react-charts';
+import { Header } from '../../components';
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis, } from '../../data/dummy';
 
 const Financial = () => {
   return (
-    <div>Financial</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg
+    rounded-3xl'>
+    <Header category='Chart' title='Hilo' />
+    <ChartComponent 
+     id='charts'
+     style= { {textAlign: "center"} }
+     primaryXAxis={FinancialPrimaryXAxis} 
+     primaryYAxis={FinancialPrimaryYAxis} 
+     legendSettings={ {visible: true} }
+     tooltip={{enable: true}}
+     title='Maximum and Minimum Rainfall'>
+      
+      <Inject services={[HiloSeries, Tooltip, Category, Crosshair, Zoom, Legend]}/>
+      <SeriesCollectionDirective>
+        <SeriesDirective dataSource={financialChartData} xName='x' yName='low' name='India' type='Hilo' low='low' high='high'>
+        </SeriesDirective>
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
   )
 }
 
